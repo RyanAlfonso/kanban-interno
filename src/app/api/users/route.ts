7077c71ee39cc9fbcc9f3f42b53f2b87b360760f
ryan.alfonso <ryan.alfonsom@outlook.com>
2025-08-25 17:16:12 -0300

    ConsertandoErroAdicionarUsuario

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,3 +1,4 @@
+
 import { getAuthSession } from "@/lib/nextAuthOptions";
 import prisma from "@/lib/prismadb";
 import { getLogger } from "@/logger";
@@ -6,7 +7,6 @@ export async function GET(req: Request) {
   const logger = getLogger("info");
   try {
     const session = await getAuthSession();
-
     if (!session?.user) return new Response("Unauthorized", { status: 401 });
 
     const users = await prisma.user.findMany({
@@ -29,3 +29,35 @@ export async function GET(req: Request) {
   }
 }
 
+export async function POST(req: Request) {
+  const logger = getLogger("info");
+  try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    const body = await req.json();
+    const { name, email, password, type } = body;
+
+    if (!name || !email || !password) {
+      return new Response("Missing required fields", { status: 400 });
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+        type,
+      },
+    });
+
+    return new Response(JSON.stringify(newUser), { status: 201 });
+  } catch (error: any) {
+    logger.error(error);
+    if (error.code === "P2002" && error.meta?.target?.includes("email")) {
+      return new Response("Este e-mail já está em uso.", { status: 409 });
+    }
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
