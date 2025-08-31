d0b077d23d987a0cc9d674bab33f419f7c91c9ea
ryan.alfonso <ryan.alfonsom@outlook.com>
2025-08-31 19:16:48 -0300

    AjusteFinal

--- src/components/Home/HomeTaskCreator.tsx
+++ src/components/Home/HomeTaskCreator.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { openTodoEditor } from "@/redux/actions/todoEditorAction";
-import { Todo } from "@prisma/client";
-import { Plus, PlusCircle } from "lucide-react";
+// --- 1. IMPORTE OS HOOKS DE NAVEGAÇÃO ---
+import { usePathname, useSearchParams } from "next/navigation";
 import { FC } from "react";
 import { useDispatch } from "react-redux";
 import { Button } from "../ui/button";
+import { PlusCircle } from "lucide-react";
 
 type HomeTaskCreatorProps = {
   columnId: string;
@@ -14,9 +15,16 @@ type HomeTaskCreatorProps = {
 
 const HomeTaskCreator: FC<HomeTaskCreatorProps> = ({ columnId, projectId }) => {
   const dispatch = useDispatch();
+  // --- 2. INICIALIZE OS HOOKS ---
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   const handleOpenDialog = () => {
-    dispatch(openTodoEditor({ columnId, projectId }, "/", "create"));
+    // --- 3. CONSTRUA A URL DE RETORNO DINÂMICA ---
+    const returnUrl = `${pathname}?${searchParams.toString()}`;
+
+    // --- 4. PASSE A URL CORRETA PARA A ACTION ---
+    dispatch(openTodoEditor({ columnId, projectId }, returnUrl, "create"));
   };
 
   return (
@@ -24,7 +32,7 @@ const HomeTaskCreator: FC<HomeTaskCreatorProps> = ({ columnId, projectId }) => {
       <Button
         variant="ghost"
         className="w-full justify-center text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200"
-        onClick={() => handleOpenDialog()}
+        onClick={handleOpenDialog} // A função agora está corrigida
       >
         <PlusCircle className="h-4 w-4 mr-2" />
         Criar Tarefa
